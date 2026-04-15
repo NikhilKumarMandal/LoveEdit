3ee2235d342c3e8838a858d18c1be829cbd8a35b
Nikhil <kumarnikhil48578@gmail.com>
2026-04-15 13:59:20 +0530

    feat: remove stats

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         </div>
       </section>
       <Faq />
-      <Stats />
+      {/* <Stats /> */}
       {/* <Testimonials /> */}
       <CTA />
       <Footer/>
